1f86b63427608850b61aa40c86a9a1c3709cc90e
Antorakk <95444282+Antorakk@users.noreply.github.com>
2022-11-28 23:09:11 +0100

    Preparation Json File + Stats Player

---
+++ VueStatsPlayer.py
@@ -0,0 +1,36 @@
+from typing_extensions import Self
+from controller.player import Player
+from customtkinter import CTkFrame,CTkLabel
+from VueNbPieceJoueur import VueNbPieceJoueur
+from VueTourJoueur import VueTourJoueur
+
+class VueStatsPlayer():
+
+    def __init__(self:Self,window,actualPlayer:Player):
+
+        self.window = window
+        self.actualPlayer = actualPlayer
+        self.frame = CTkFrame(master=self.window,fg_color="white")
+        self.UI()
+
+    def UI(self:Self):
+
+        self.frame.grid_rowconfigure(0,weight=1)
+        self.frame.grid_rowconfigure(1,weight=1)
+        self.frame.grid_columnconfigure(0,weight=1)
+
+        self.label = CTkLabel(
+            master=self.frame,
+            text="Joueur ", 
+            text_font=("Roboto Medium", 40)
+        )
+        self.label.grid(row=0,column=0)
+
+        self.tourJoueur = VueTourJoueur(self.frame)
+        self.nbPiecesPlayer = VueNbPieceJoueur(self.frame,self.actualPlayer)
+
+        self.frame.place(x=120,y=10)
+
+
+
+    
