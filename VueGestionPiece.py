e9dc0e6fb0333bcec97f0f0d2d70e80b8b20205b
Antorakk <95444282+Antorakk@users.noreply.github.com>
2022-11-28 23:10:06 +0100

    New Drag & Drog 100% fix

--- VueGestionPiece.py
+++ VueGestionPiece.py
@@ -17,39 +17,42 @@ class VueGestionPiece:
         self.master = master
         self.rotateButton = None
     
-    def getXSouris(self)->int:
+    def getXSouris(self:Self)->int:
         return self.window.winfo_pointerx() - self.window.winfo_rootx()
 
-    def getYSouris(self)->int:
+    def getYSouris(self:Self)->int:
         return self.window.winfo_pointery() - self.window.winfo_rooty()
 
 
-    def onMotion(self,e):
+    def onMotion(self:Self,e,width,heigh):
         if self.canvas_piece:
-            self.new_x = self.getXSouris()
-            self.new_y = self.getYSouris()
+            self.new_x = self.getXSouris() - width//2
+            self.new_y = self.getYSouris() - heigh//2
             self.canvas_piece.place(x=self.new_x,y=self.new_y)  
  
-    def onDrop(self,e):
-        self.abs_x = self.getXSouris()
-        self.abs_y = self.getYSouris()
+    def onDrop(self:Self,e,width,heigh):
+        self.abs_x = self.getXSouris() - width//2
+        self.abs_y = self.getYSouris() - heigh//2
 
-        if 0<=self.abs_x<=600 and 0<=self.abs_y<=600 :
+        if 60<=self.abs_x<=660 and 150<=self.abs_y<=750 :
+
             x_round = self.round_down(self.abs_x)
             y_round = self.round_down(self.abs_y)
+
             if self.canvas_piece:
                 self.canvas_piece.destroy()
-            self.emitToGrille(x_round,y_round)
+            self.emitToGrille(x_round-60,y_round-150)
 
-    def emitToGrille(self,x,y):
-        self.master.callbackPiece(self.file,x,y,self.rotateButton.angle)
+    def emitToGrille(self:Self,x,y):
+        if self.rotateButton:
+            self.master.callbackPiece(self.file,x,y,self.rotateButton.angle)
 
-    def round_down(self,n, decimals=2)->int:
+    def round_down(self:Self,n, decimals=2)->int:
         multiplier = 10 ** decimals
         result =  int(math.floor(n * multiplier) / multiplier)
         return (result//30)*30
 
-    def addImageToGrid(self,f,widget_location):
+    def addImageToGrid(self:Self,f,widget_location):
         if self.canvas_piece:
             self.canvas_piece.destroy()
         if self.rotateButton:
@@ -60,11 +63,12 @@ class VueGestionPiece:
         img=Image.open(f)
         w,h=img.size 
 
+
         self.canvas_piece = Canvas(self.window, width=w, height=h, bd=0, highlightthickness=0, relief='ridge')
         self.img = ImageTk.PhotoImage(file=f)
         self.png_piece = self.canvas_piece.create_image(0,0,image=self.img,anchor = "nw" )   
        
         self.canvas_piece.place(x=widget_location[0],y=widget_location[1])
-        self.canvas_piece.bind('<B1-Motion>',self.onMotion)
-        self.canvas_piece.bind('<ButtonRelease-1>',self.onDrop)
+        self.canvas_piece.bind('<B1-Motion>',lambda e :self.onMotion(e,w,h))
+        self.canvas_piece.bind('<ButtonRelease-1>',lambda e : self.onDrop(e,w,h))
         self.rotateButton = BoutonCanvas(self.canvas_piece,self.png_piece,self.window,f)
