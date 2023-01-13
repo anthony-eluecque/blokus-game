d4a95f38fc5b3a148a53d8e12620ffaca58bb447
Antorakk <95444282+Antorakk@users.noreply.github.com>
2023-01-13 10:33:11 +0100

    delete useless method and use window's utils

--- src/Main.py
+++ src/Main.py
@@ -1,6 +1,7 @@
 # -*- encoding:utf-8 -*-
 from core.Core import Core
 from customtkinter import CTk
+from utils.window_utils import _resizeWindow
 
 """
     Main class. Responsible for running the application.
@@ -8,28 +9,14 @@ from customtkinter import CTk
 class Main:
     @staticmethod    
     def run():
-        # try:
-
+        try:
             window = CTk()
-            print(window)
-            Main._resizeWindow(window,700,700)
+            _resizeWindow(window,700,700)
             app = Core.openController("Home",window)
             app.main()
             window.mainloop()
-        # except Exception as e:
-        #     print(str(e))
-
-    @staticmethod
-    def _resizeWindow(window,width : int, heigth : int):
-        
-        window.resizable(width=False, height=False)
-        _screen_width : int = window.winfo_screenwidth()
-        _screen_height : int  = window.winfo_screenheight()
-        x : float = (_screen_width/2) - (width/2)
-        y : float = (_screen_height/2) - (heigth/2)
-
-        window.geometry('%dx%d+%d+%d' % (width, heigth, x, y))
-        window.geometry(str(width) + 'x' + str(heigth))
+        except Exception as e:
+            print(str(e))
 
 if __name__ == '__main__':
     Main.run()
