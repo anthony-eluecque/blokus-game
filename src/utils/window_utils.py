81ecbc76046650ba8dedb93c1c332d1472e15372
Antorakk <95444282+Antorakk@users.noreply.github.com>
2023-01-13 10:57:05 +0100

    add methods for frame

--- src/utils/window_utils.py
+++ src/utils/window_utils.py
@@ -1,3 +1,6 @@
+from customtkinter import CTk
+from tkinter import Frame
+
 def _resizeWindow(window,width:int,heigth:int):
 
     window.resizable(width=False, height=False)
@@ -8,3 +11,13 @@ def _resizeWindow(window,width:int,heigth:int):
 
     window.geometry('%dx%d+%d+%d' % (width, heigth, x, y))
     window.geometry(str(width) + 'x' + str(heigth))
+
+def _deleteChilds(window : CTk):
+    for child in window.winfo_children():
+            child.destroy()
+
+def _createFrame(window: CTk , longueur = 700 , hauteur = 1000) -> Frame:
+    frame =  Frame(window,width=longueur,height=hauteur)
+    frame.pack()
+    frame.pack_propagate(False)
+    return frame
