8b820af602f283dc4f5cb42ed73ee4315baf9fd8
Antorakk <95444282+Antorakk@users.noreply.github.com>
2023-01-16 10:12:05 +0100

    Controller utils

---
+++ src/utils/controller_utils.py
@@ -0,0 +1,8 @@
+from core.Controller import Controller
+from core.Core import Core
+
+def _openController(oldView,controllerName,window):
+    oldView.close()
+    c = Core.openController(controllerName,window)
+    return c.main()
+
