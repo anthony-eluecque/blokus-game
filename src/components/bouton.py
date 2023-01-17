8b16d4eac486841e1ab096a0424fc9a833877b1b
Antorakk <95444282+Antorakk@users.noreply.github.com>
2023-01-17 15:58:08 +0100

    Bouton top class feature

---
+++ src/components/bouton.py
@@ -0,0 +1,42 @@
+from components.soundclass import Sound
+from tkinter import Button
+from customtkinter import CTk
+from tkinter import PhotoImage
+
+class Bouton(Button,Sound):
+
+    def __init__(
+        self,window:CTk,view,xpos,ypos,
+        file:str|None = None,command=None, 
+        width = 250, heigth = 250,
+        text="template",son:str|None=None,*args,**kwags)->None:
+
+        self.command = command
+        if file:
+            self.image = PhotoImage(file = file)
+            super().__init__(window,image=self.image,text=text,bg="white",highlightthickness=0,bd=0,border=0)
+        else:
+            super().__init__(window,text=text,bg="white",highlightthickness=0,bd=0,border=0)
+        
+        self.window = window
+        self.view = view 
+        self.width = width
+        self.heigth = heigth
+        super().configure(width=width,height=heigth,command=self.callbackButton)
+        super().place(x=xpos,y=ypos)
+
+        if son:
+            self.sound = Sound(son)
+
+    def callbackButton(self):
+        if self.command:
+            if self.sound:
+                self.sound.play()
+            self.command()
+            
+
+
+
+        
+
+
