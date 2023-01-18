475daae0add9a4e3cd97b4cd980edbc1e713c415
Okane <anatole.giniaux@sfr.fr>
2023-01-18 10:00:00 +0100

    Translated soundclass
    (removed a space in bouton.py)

--- src/components/soundclass.py
+++ src/components/soundclass.py
@@ -1,54 +1,51 @@
 # imports
 from os import environ
-environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1' # Remove pygame alert
+environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1' # Retire l'alerte de pygame dans le terminal
 from pygame import mixer
 
 class Sound:
     def __init__(self, sound: str) -> None:
-        # Init mixer
+        # Initialisation du mixer
         mixer.init()
-        # Constant
+        # Constante
         self.SOUND_LIST: list[str] = ["background", "select", "drop", "button"]
-        # Sound
+        # Son
         self.sound = sound.lower()
 
     def play(self):
-        """Play a sound
-
-        Args:
-            sound (str): Sound to play
+        """Joue un son
         """
-        # Stop sound if it is already playing
+        # Coupe le son si il y en a un en train d'être joué
         mixer.music.stop()
 
-        # Verifying if the sound exist
+        # Verification si le son existe
         if(self.sound not in self.SOUND_LIST):
             return "Sound does not exist"
 
-        # Load sound
+        # Chargement du son
         mixer.music.load("././media/sounds/" + self.sound + ".wav")
 
-        # If the sound to play is the background music
+        # Si le son à jouer est la musique de fond 
         if(self.sound == "background"):
-            self.setVolume(0.3) # Down the volume
-            mixer.music.play(loops=-1) #Play the music indefinitely
+            self.setVolume(0.3) # On baisse le son
+            mixer.music.play(loops=-1) # Et on joue la musique indéfiniment
         else:
-            self.setVolume(1) # Up the volume
-            mixer.music.play() # Play the sound
+            self.setVolume(1) # On monte le son
+            mixer.music.play() # On joue le son
 
     def setVolume(self, i: float) -> None:
-        """Change the volume of the sound
+        """ Changer le volume d'un son
 
         Args:
-            i (int): New volume 
+            i (int): Nouveau volume
         """
-        assert i>=0 and i<=1, "The volume need to be between 0 and 1"
+        assert i>=0 and i<=1, "Le volume doit être entre 0 & 1"
         mixer.music.set_volume(i)
 
 if __name__ == '__main__':
     from customtkinter import CTk 
 
     w = CTk()
-    a = Sound("select")
+    a = Sound("background")
     a.play()
     w.mainloop()
