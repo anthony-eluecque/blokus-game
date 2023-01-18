a550b6482237f4da9b3e8d8242f7ade2db79b29f
Antorakk <95444282+Antorakk@users.noreply.github.com>
2023-01-18 09:17:53 +0100

    remove useless method

--- src/controllers/ScoreController.py
+++ src/controllers/ScoreController.py
@@ -11,23 +11,6 @@ class ScoreController(Controller):
         
         self.window = window
         self.scoreView = self.loadView("score",self.window)
-        # self.core = Core()
-
-        # self._sortClassement()
-        # self.scoreView._makeClassement(classement)
-
-    def _sortClassement(joueurs):
-        classement = {}
-        for joueur in joueurs :
-            for numPiece in joueur.pieces.pieces_joueurs:
-                piece = joueur.jouerPiece(numPiece-1)
-                for line in piece:
-                    for square in line:
-                        if square == 1:
-                            joueur.removeScore()
-            classement[joueur.couleur]=joueur.score
-
-        print(classement)
 
     def main(self):
         self.scoreView.main()
