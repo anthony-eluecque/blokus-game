bcf0f336c2ca763873a534f46abbd2261a72ff22
Antorakk <95444282+Antorakk@users.noreply.github.com>
2023-01-18 09:11:43 +0100

    add docstring for soundbutton

--- src/components/SoundButton.py
+++ src/components/SoundButton.py
@@ -4,7 +4,11 @@ from components.bouton import Bouton
 from components.soundclass import Sound
 
 class SoundButton(Bouton, Sound):
-    def __init__(self, window: CTk, view, x_coord: int|float, y_coord: int|float) -> None:
+    """Classe héritant des Classes Bouton et Sound 
+    Permet une surcouche pour la création du bouton du menu très particulier et unique
+    Respecte les principes SOLID.
+    """
+    def __init__(self, window: CTk, view, x_coord: int, y_coord: int) -> None:
         self.window = window
         self.button: Button
         self.bgButton: PhotoImage
@@ -12,31 +16,39 @@ class SoundButton(Bouton, Sound):
         self.main(x_coord,y_coord)
     
     def _createWidget(self)->None:
+        """
+        Fonction permettant la création du Bouton
+        """
         self.bgButton = PhotoImage(file="./media/assets/sound_on.png")
         self.img: int = 1 # Current img
         self.button = Button(master = self.window, image = self.bgButton, text="", bg='white', highlightthickness=0, bd=0, border=0, command=self.changeStatus, width=50, height=50)
 
-    def _placeWidget(self, x_pos, y_pos)->None:
+    def _placeWidget(self, x_pos:int, y_pos:int)->None:
+        """Fonction permttant de placer le bouton dans son parent (ici la fenêtre)
+
+        Args:
+            x_pos (int): la position en X sur la fenêtre.
+            y_pos (int): la position en Y sur la fenêtre.
+        """
         self.button.place(x=x_pos, y=y_pos)
 
     def changeStatus(self)->None:
-        # If the sound is on
+        """
+        Fonction permettant de changer l'affichage de l'image sur le bouton
+        ON -> OFF
+        OFF -> ON
+        """
         if(self.img==1):
-            # New button image
             self.bgButton = PhotoImage(file="./media/assets/sound_off.png")
-            # Changing button image
             self.button.configure(image=self.bgButton)
-            # Changing current img
             self.img=0
-            # Set the volume to 0
             super().setVolume(0)
-        # If the sound is off
         else:
             self.bgButton = PhotoImage(file="./media/assets/sound_on.png")
             self.button.configure(image=self.bgButton)
             self.img=1
             super().setVolume(0.3)
 
-    def main(self, x_pos, y_pos):
+    def main(self, x_pos, y_pos)->None:
         self._createWidget()
         self._placeWidget(x_pos,y_pos)
