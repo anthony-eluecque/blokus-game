f394b5d2ce3e41efc914320058869d158d250353
Antorakk <95444282+Antorakk@users.noreply.github.com>
2023-01-24 15:10:17 +0100

    create frame card for players

--- src/views/GameParamView.py
+++ src/views/GameParamView.py
@@ -1,6 +1,6 @@
 from tkinter import Label
 from views.View import View
-from customtkinter import CTk, CENTER, CTkTextbox, CTkLabel, CTkFont
+from customtkinter import CTk, CENTER,CTkImage, CTkTextbox, CTkLabel, CTkFont
 from PIL import Image, ImageTk
 from typing_extensions import Self
 from utils.window_utils import _resizeWindow, _deleteChilds, _createFrame
@@ -16,22 +16,37 @@ class GameParamView(View):
         self.paramController = controller
         self.window = window
 
-    def _makeFrame( self ):
+    def _makeFrame(self):
         self.mainFrame = _createFrame( self.window, 1300, 800 )
 
-    def _makeWindow( self ):
+    def _makeWindow(self):
         self.backgroundImage = Image.open( "./media/assets/background_gameparam.png" )
         self.background = ImageTk.PhotoImage( self.backgroundImage )
         self.gameParamTitle = Label( self.mainFrame, text="", image = self.background, bd = 0 )
 
-    def _configWidget( self ):
-        self.gameParamTitle.place( x = 0,y = 0 )
+    def _configWidget(self):
+        self.gameParamTitle.place(x = 0,y = 0)
         
-    def __makeButtons( self ):
+    def __makeButtons(self):
         self.settingsBt: Bouton = Bouton( self.window, self, 56, 12, width=65, heigth=65, file="./media/assets/button_settings.png", son="button" )
         self.launchBt: Bouton = Bouton( self.window, self, 543.5, 345.5, width=207, heigth=105, file="./media/assets/button_launch.png", son="button", command=self.paramController.btn_play )
         self.retourBt: Bouton = Bouton( self.window, self, 40, 743, width=570, heigth=48, file="./media/assets/button_retour.png", son="button", command=self.paramController.btn_retour )
         self.reglesBt: Bouton = Bouton( self.window, self, 695, 743, width=570, heigth=48, file="./media/assets/button_regles.png", son="button", command=self.paramController.btn_regles )
+
+    def __makeCardPlayer(self):
+
+        # Values : x positions[key][0] ; y positions[key][1]
+        positions = {"1":[200,100],"2":[850,100],"3":[200,460],"4":[850,460]}
+        self.labelPlayers = []
+        self.bgImage = CTkImage(Image.open("./media/assets/player_frame_param.png"), size=(250,50))
+        
+        for items in positions.values():
+            self.player = CTkLabel(master = self.mainFrame,text="" , image=self.bgImage)
+            self.player.place(x=items[0],y=items[1])
+            self.labelPlayers.append(self.player)
+    
+    def __makeEntry(self):
+        pass
 
     def main( self, longueur = 1300, hauteur = 800 ):
         _resizeWindow( self.window, longueur, hauteur )
@@ -39,6 +54,8 @@ class GameParamView(View):
         self._makeWindow()
         self._configWidget()
         self.__makeButtons()
+        
+        self.__makeCardPlayer()
         
     def close( self ):
         _deleteChilds( self.window )
