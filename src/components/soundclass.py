cb727b708dba1d8ebd505b6546e1333404270976
Okane <anatole.giniaux@sfr.fr>
2023-01-25 10:58:46 +0100

    Edited & optimized soundclass.py

--- src/components/soundclass.py
+++ src/components/soundclass.py
@@ -2,6 +2,7 @@
 from os import environ
 environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1' # Retire l'alerte de pygame dans le terminal
 from pygame import mixer
+from utils.sound_utils import SOUND_LIST, SOUND_VOLUME
 
 class Sound:
     """
@@ -10,33 +11,33 @@ class Sound:
     def __init__(self, sound: str) -> None:
         # Initialisation du mixer
         mixer.init()
-        # Constante
-        self.sounds: list[str] = ["background", "select", "drop", "button", "testing"]
         # Son
         self.sound = sound.lower()
+        # Vérification si le son existe
+        assert self.sound in SOUND_LIST, "Sound does not exist"
+        # Volume
+        self.volume: int|float
 
     def play(self):
         """Joue un son
         """
         # Coupe le son si il y en a un en train d'être joué
-        mixer.music.stop()
-
-        # Verification si le son existe
-        if(self.sound not in self.sounds):
-            return "Sound does not exist"
+        mixer.music.stop()        
 
         # Chargement du son
         mixer.music.load("././media/sounds/" + self.sound + ".wav")
 
         # Si le son à jouer est la musique de fond 
         if(self.sound == "background"):
-            self.setVolume(0) # On baisse le son
+            self.volume = SOUND_VOLUME["sound"]
+            self.setVolume(self.volume) # On baisse le son
             mixer.music.play(loops=-1) # Et on joue la musique indéfiniment
         else:
-            self.setVolume(1) # On monte le son
+            self.volume = SOUND_VOLUME["sfx"]
+            self.setVolume(self.volume) # On monte le son
             mixer.music.play() # On joue le son
 
-    def setVolume(self, i: float) -> None:
+    def setVolume(self, i: int|float) -> None:
         """Changer le volume d'un son
 
         Args:
