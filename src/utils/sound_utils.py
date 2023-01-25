e2b0d69f9be8ea9eabae69a7c3c1c8690b1992ef
Okane <anatole.giniaux@sfr.fr>
2023-01-25 20:21:46 +0100

    Create sound_utils.py

---
+++ src/utils/sound_utils.py
@@ -0,0 +1,31 @@
+from json import load, dump
+
+with open("./src/utils/soundVolume.json", 'r') as f:
+    SOUND_VOLUME: dict[str, float | int] = load(f)
+
+# print(SOUND_VOLUME)
+# print(list(SOUND_VOLUME.keys()))
+# print(SOUND_VOLUME["music"])
+
+def _editValue(sound: str, volume: int|float) -> None:
+    """Permet de modifier le volume d'un son de manière à récupérer cette valeur modifiée dans tout le programme (passée dans un json)
+
+    Args:
+        sound (str): Le son dont il faut modifier le volume
+        volume (int | float): Le volume qu'il faut attribuer au son
+    """
+    assert sound in list(SOUND_VOLUME.keys()), f"'{sound}' is not a value whose sound can be modified"
+
+    SOUND_VOLUME[sound] = volume
+
+    with open("./src/utils/soundVolume.json", 'w') as d:
+        dump(SOUND_VOLUME, d)
+
+def _getValues() -> dict[str, float|int]:
+    """Permet d'obtenir les sons et leur volume attribué 
+
+    Returns:
+        dict[str, float|int]: Dictionnaire des sons et leur volume 
+                              ex: {"son" : volume}
+    """ 
+    return SOUND_VOLUME
