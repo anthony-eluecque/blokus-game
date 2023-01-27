b85a7b3a4813474282ef9d8316806dc32b0bada6
Antorakk <95444282+Antorakk@users.noreply.github.com>
2023-01-27 14:11:30 +0100

    First IA pour test (ez mode)

--- src/controllers/GameController.py
+++ src/controllers/GameController.py
@@ -149,13 +149,45 @@ class GameController(Controller):
         _openController(self.gameView, "Home", self.window)
 
     def main(self):
-        self.startGame()    
         self.gameView.main()
+        self.startGame()    
         self.gameView.update(self.actualPlayer, self.index)
         #self.loadMap()
 
     def IA(self):
-        print("Oui bonjour c'est moi benjamin")
         
+        from random import randint
+        # print("Oui bonjour c'est moi benjamin")
+        done = False
+        grille = self.plateau.getTab()
+        for i,elem in enumerate(self.plateau.getTab()):
+            for j,case in enumerate(elem):
+                if case == self.index:
+                    done = True
+
+        # Partie du début de game
+        if not done:
+        
+            x,y = self.actualPlayer.getPositionDepart()
+
+            indicePiece = randint(0,20)
+            piece = self.actualPlayer.jouerPiece(indicePiece)
+            pieceBlokus = coordsBlocs(piece,x,y)
+
+            print(pieceBlokus)
+
+            while not validPlacement(piece,y,x,self.plateau,self.actualPlayer):   
+                indicePiece = randint(0,20)
+                piece = self.actualPlayer.jouerPiece(indicePiece)
+                pieceBlokus = coordsBlocs(piece,x,y)
+
+            # Classement
+            cheminFichierPiece = "./media/pieces/" + self.actualPlayer.getCouleur().upper()[0] + "/1.png"
+
+            for ypos,xpos in pieceBlokus:
+                self.gameView._addToGrid(cheminFichierPiece,xpos,ypos)
+                self.plateau.setColorOfCase(ypos,xpos,self.index)
+
+            self.actualPlayer.hasPlayedPiece(indicePiece)
 
         self.nextPlayer()
