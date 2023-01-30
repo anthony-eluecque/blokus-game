481870f7fa58688b8e6bb55c26a41b8e6e4a3223
Okane <anatole.giniaux@sfr.fr>
2023-01-30 16:57:47 +0100

    Update lang_utils.py

--- src/utils/lang/lang_utils.py
+++ src/utils/lang/lang_utils.py
@@ -12,7 +12,7 @@ def _editLang(new_lang: str) -> None:
     """
     assert new_lang.upper() in LANGUAGES, f"{new_lang} n'est pas une langue valide"
 
-    LANG["lang"] = new_lang
+    LANG["lang"] = new_lang.upper()
 
     with open("./src/utils/lang/lang.json", 'w') as d:
         dump(LANG, d)
@@ -25,3 +25,20 @@ def _getLang() -> str:
     """
     return LANG["lang"]
 
+CURRENT_LANG: str = _getLang()
+
+def _getValue(value: str | int) -> str | list[str]:
+    """Permet de récupérer une valeur dans la langue du jeu
+
+    Args:
+        value (str | int): L'élément dont il faut récupérer la valeur
+
+    Returns:
+        str | list[str]: La valeur de l'élément dans la langue
+                         ex: _getValue("settingsTitle")
+                             "Paramètres" / "Settings"
+    """
+    dico: dict = LANG_CONTENT[CURRENT_LANG]
+    assert value in list(dico.keys())
+
+    return dico[value]
