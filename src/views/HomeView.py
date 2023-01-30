b170ad56b9f8c69ff0de733328019aa1699b078e
Okane <anatole.giniaux@sfr.fr>
2023-01-30 13:35:06 +0100

    Updated main menu
    Deleted SoundButton.py (is not used)

--- src/views/HomeView.py
+++ src/views/HomeView.py
@@ -6,7 +6,6 @@ from typing_extensions import Self
 import os
 from utils.window_utils import _resizeWindow,_deleteChilds,_createFrame
 from components.bouton import Bouton
-from components.SoundButton import SoundButton
 
 class HomeView(View):
     """
@@ -26,8 +25,6 @@ class HomeView(View):
         self.label.place(x = 0,y = 0)  
 
     def _UI(self : Self):
-        self.testButton : Bouton = Bouton(self.window, self, 20, 20, width=370, heigth=49, file="./media/assets/button_play.png", son="button", command=self.homeController.btnSettings)
-
         self.playButton: Bouton = Bouton(self.window, self, 165, 315, width=370, heigth=49, file="./media/assets/button_play.png", son="button", command=self.homeController.btnPlay)
 
         self.rulesButton: Bouton = Bouton(self.window, self, 165, 380, width=370, heigth=49, file="./media/assets/button_rules.png", son="button", command=self.homeController.btnRules)
@@ -36,12 +33,11 @@ class HomeView(View):
 
         self.leaveButton: Bouton = Bouton(self.window, self, 165, 510, width=370, heigth=49, file="./media/assets/button_leave.png", son="button", command=self.window.destroy)
                 
+        self.testButton : Bouton = Bouton(self.window, self, 85, 560, width=50, heigth=50, file="./media/assets/settings.png", son="button", command=self.homeController.btnSettings)
+
         self.backgroundImage = Image.open(os.path.join("./media/assets/carre.png"))
         self.background = ImageTk.PhotoImage(self.backgroundImage)
         self.label = Label(self.mainFrame, image = self.background, bd = 0)   
-
-        self.soundButton: SoundButton = SoundButton(self.window, self, 85, 560)
-
 
     def main(self, longueur = 700 ,hauteur = 700):
         _resizeWindow(self.window,longueur,hauteur)
