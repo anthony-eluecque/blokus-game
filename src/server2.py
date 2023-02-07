5e3e7814f5f3e6fc3e251b2c30c45ac3de09d893
Antorakk <95444282+Antorakk@users.noreply.github.com>
2023-02-07 15:16:44 +0100

    server with thread

---
+++ src/server2.py
@@ -0,0 +1,36 @@
+from tkinter import *
+from socket import *
+from threading import *
+from tkinter import scrolledtext
+
+    
+class App(Thread):
+
+  def __init__(self):
+    Thread.__init__(self)
+    server = socket()
+    server.bind(('0.0.0.0', int(input("Port: "))))
+    server.listen(5)
+    counter = 0
+    self.joueurs = []
+
+    while counter<2:
+        self.client,self.addr = server.accept()
+        self.joueurs.append([self.client,self.addr])
+        counter+=1
+
+    print("c tipar")
+    for client in self.joueurs:
+        text = "start"
+        client[0].send(text.encode('utf-8'))
+
+    self.clienttext="start"
+
+    while 1:
+        try:
+            text = server.recv(1024)
+            if not text: break
+        except:
+            break
+
+app = App().start()
