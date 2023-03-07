7457d891cd347fda3031e46cb263ba10d4409b3a
Antorakk <95444282+Antorakk@users.noreply.github.com>
2023-03-07 22:11:28 +0100

    manager database + addPoints methods

--- src/utils/data_utils.py
+++ src/utils/data_utils.py
@@ -20,17 +20,28 @@ class jsonManager():
 class dataGame():
 
     def __init__(self) -> None:
-        
+        date = datetime.datetime.now()
+        heure = str(date.hour) + "h" + str(date.minute)
+        date = str(date.day) + "-" + str(date.month) + "-" + str(date.year)
+        self.id = "1"
+
         if os.path.exists('./src/models/data.json'):
             self.data = jsonManager.readJson()
+            self.id = str(int(list(self.data["parties"].keys())[-1]) + 1)
+            self.data["parties"][self.id] = {'date': date, 'heure': heure, 'bleu': {'score': 0, 'historique_placement': []}, 'rouge': {'score': 0, 'historique_placement': []}, 'vert': {'score': 0, 'historique_placement': []}, 'jaune': {'score': 0, 'historique_placement': []}}
+            jsonManager.writeJson(self.data)
         else:
-            date = datetime.datetime.now()
-            heure = str(date.hour) + "h" + str(date.minute)
-            date = str(date.day) + "-" + str(date.month) + "-" + str(date.year)
+            jsonManager.writeJson(jsonManager.readJson("./src/models/template_data.json"))
             self.data = jsonManager.readJson()
             self.data["parties"]["1"]["heure"] = heure
             self.data["parties"]["1"]["date"] = date
             jsonManager.writeJson(self.data)
+
+    
+    def addPoints(self,couleur:str,taillePiece:int)->None:
+        self.data = jsonManager.readJson()
+        self.data["parties"][self.id][couleur.lower()]["score"]+=taillePiece
+        jsonManager.writeJson(self.data)
 
 
 
