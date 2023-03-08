6306f7f529e33822b98109bf20ecb43db14c5359
Antorakk <95444282+Antorakk@users.noreply.github.com>
2023-03-08 12:52:46 +0100

    View for stats (init)

--- src/views/StatsView.py
+++ src/views/StatsView.py
@@ -4,6 +4,8 @@ from utils.data_utils import jsonManager
 from customtkinter import CTk, CTkImage, CTkLabel
 from PIL import Image
 from tkinter import Label
+from components.stats.gamehistorique import gameHistorique
+from components.bouton import Bouton
 
 class StatsView(View):
     
@@ -23,13 +25,34 @@ class StatsView(View):
     def _makeTitle(self)->None:
         self.mainTitle = Label( self.mainFrame, text="", image = self.background, bd = 0 )
 
-    def createWidgets(self)->None: ...
+    def createWidgets(self)->None:
+        self.data = jsonManager.readJson()
+
+        for idPartie in self.data["parties"]:
+            print(idPartie)
+            # gameHistorique(self.statsController.showWidget)
+            
+            
+            bgImage = CTkImage(Image.open("./media/assets/bg_histo.png"), size=(600, 55))
+            self.bg = CTkLabel(self.window, text="", image = bgImage)
+            self.bg.place(x = 80, y = 180)
+
+            self.labelTime = CTkLabel(self.window,text=self.data["parties"][idPartie]["date"] + " | " + self.data["parties"][idPartie]["heure"], bg_color='white' ,text_color="black")
+            self.labelTime.place(x=120,y=190)
+
+            self.labelWinner = CTkLabel(self.window,text=self.data["parties"][idPartie]["gagnant"],bg_color="white",text_color="black")
+            self.labelWinner.place(x=350,y=190)
+
+            self.button = Bouton(self.window,view=self,xpos=560,ypos=180,width=100,heigth=45,file="./media/assets/afficher.png",command=self.statsController.showWidget)
+
+
 
 
     def main(self,width=1300,heigth=800)->None:
         _resizeWindow(self.window, width, heigth)
         self._makeFrame()
         self._makeBackground()
+        self.createWidgets()
 
 
     def close(self)->None:
