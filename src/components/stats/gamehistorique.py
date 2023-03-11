0a0687d99d7492ee9a136b822b46cc7b5eb0f40c
Antorakk <95444282+Antorakk@users.noreply.github.com>
2023-03-11 14:56:15 +0100

    update component gameHistorique

--- src/components/stats/gamehistorique.py
+++ src/components/stats/gamehistorique.py
@@ -1,6 +1,7 @@
 from tkinter import PhotoImage,Button
-from customtkinter import CTk,CTkLabel
+from customtkinter import CTk,CTkLabel,CTkImage
 from components.bouton import Bouton
+from PIL import Image
 
 
 class gameHistorique():
@@ -9,13 +10,23 @@ class gameHistorique():
     
         self.window = window
         self.view = view
-        self.id = idPartie
+        
+        self.id = str(idPartie)
 
         self.partie = dictPartie
-        
-        
-        self.labelHeureDate = CTkLabel(self.window,text=self.partie["date"]+ " | " + self.partie["heure"])
-        self.button = Bouton(window,view=self.view,xpos=xcoord,ypos=ycoord,width=50,heigth=30,command=command)
+        bgImage = CTkImage(Image.open("./media/assets/bg_histo.png"), size=(600, 55))
+        self.bg = CTkLabel(self.window, text="", image = bgImage)
+        self.bg.place(x = xcoord, y = ycoord)
+
+        self.labelTime = CTkLabel(self.window,text=self.partie["date"]+ " | " + self.partie["heure"])
+        self.labelTime.place(x=xcoord+40,y=ycoord+10)
+
+        self.labelWinner = CTkLabel(self.window,text=self.partie["gagnant"],bg_color="white",text_color="black")
+        self.labelWinner.place(x=xcoord+270,y=ycoord+10)
+
+        self.button = Bouton(self.window,view=self.view,file="./media/assets/afficher.png",xpos=xcoord+480,ypos=ycoord,width=100,heigth=48,command=command,son="button")
 
 
+
+    # x_pos+40 , y_pos+ 10 , i+
 
