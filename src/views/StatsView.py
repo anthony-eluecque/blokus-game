4c295e655814a58e79aa3a52f1cafd7896fcd949
Antorakk <95444282+Antorakk@users.noreply.github.com>
2023-03-11 17:08:43 +0100

    edit view for adding resume game

--- src/views/StatsView.py
+++ src/views/StatsView.py
@@ -2,7 +2,7 @@ from views.View import View
 from utils.window_utils import _resizeWindow, _createFrame, _deleteChilds
 from utils.data_utils import jsonManager
 from customtkinter import CTk, CTkImage, CTkLabel
-from PIL import Image
+from PIL import Image,ImageTk
 from tkinter import Label
 from components.stats.gamehistorique import gameHistorique
 from components.bouton import Bouton
@@ -14,15 +14,15 @@ class StatsView(View):
         self.window = window
         self.statsController = controller
 
-    def _makeFrame(self):
-        self.mainFrame = _createFrame(self.window, 1300, 800)
+    def _makeFrame(self,width,heigth) -> None:
+        self.mainFrame = _createFrame(self.window, width, heigth)
 
-    def _makeBackground(self)->None:
-        self.bgImage = CTkImage(Image.open("./media/assets/bgStats.png"), size=(1300, 800))
+    def _makeBackground(self,xsize,ysize,file="./media/assets/bgStats.png")->None:
+        self.bgImage = CTkImage(Image.open(file), size=(xsize, ysize))
         self.bg = CTkLabel(self.window, text="", image = self.bgImage)
         self.bg.place(x = 0, y = 0)
 
-    def _makeTitle(self)->None:
+    def _makeTitle(self) -> None:
         self.mainTitle = Label( self.mainFrame, text="", image = self.background, bd = 0 )
 
     def createWidgets(self)->None:
@@ -35,15 +35,42 @@ class StatsView(View):
         for idPartie in self.data["parties"]:
             widget = gameHistorique(self.window,self,xcoord=xpos,ycoord=ypos,idPartie=idPartie,dictPartie=self.data["parties"][idPartie],command=self.statsController.showWidget)
             self.widgets.append(widget)
-            ypos += 100
+            ypos += 65
+
+    
+    def openDetailGame(self,idPartie) -> None:
+        _deleteChilds(self.window)
+        _resizeWindow(self.window,500,600)
+        self._makeFrame(500,600)
+        self._makeBackground(500,600,"./media/assets/bgDetailPartie.png")
+        self.banners = []
+
+        colors = ["bleu","rouge","vert","jaune"]
+        y = 100
+        for color in colors:
+            banner = self.makeBanner(("./media/assets/banner"+color+".png"),xpos=50,ypos=y)
+            self.banners.append(banner)
+            y+=110
+        self.backStats: Bouton = Bouton(self.window, self, 150, 540, width=206, heigth=49, file="./media/assets/buttun_rules_return.png", son="button", command=self.statsController.backToStats)
 
 
-    def main(self,width=1300,heigth=800)->None:
-        _resizeWindow(self.window, width, heigth)
-        self._makeFrame()
-        self._makeBackground()
+
+
+    def makeBanner(self,file,xpos,ypos):
+        self.img = CTkImage(Image.open(file),size=(400,40))
+        label = CTkLabel(master=self.window,text="",image=self.img)
+        label.place(x=xpos,y=ypos)
+        return label
+        
+        
+
+
+    def main(self, width=1300, height=800) -> None:
+        _resizeWindow(self.window, width, height)
+        self._makeFrame(width,height)
+        self._makeBackground(1300,800)
         self.createWidgets()
 
 
-    def close(self)->None:
+    def close(self) -> None:
         _deleteChilds(self.window)
