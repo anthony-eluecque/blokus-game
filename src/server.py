93371a0ab3f7d6622416dd4816df47bb411c69dd
Fredericdrnl <114562911+Fredericdrnl@users.noreply.github.com>
2023-03-22 16:03:30 +0100

    chnage server

--- src/server.py
+++ src/server.py
@@ -2,31 +2,39 @@ from tkinter import *
 from socket import *
 from threading import *
 from tkinter import scrolledtext
+from Main import Main
+from controllers.GameController import GameController
 
     
-class App(Thread):
+class Server(Thread):
 
     def __init__(self):
         
         Thread.__init__(self)
-        server = socket()
-        server.bind(('0.0.0.0', 3000))
-        server.listen(5)
-        counter = 0
+        self.server = socket()
+        self.server.bind(('0.0.0.0', 3000))
+        self.server.listen(5)
+        self.counter = 0
         self.joueurs = []
 
-        print("En attente de la connection de 2 joueurs..\n")
-        while counter<2:
-            self.client,self.addr = server.accept()
-            self.joueurs.append([self.client,self.addr,counter])
-            counter+=1
-            print(f"Il y a {counter} joueur(s) connectés")
+    def sendAllPeople(self, mess):
+        for joueur in self.joueurs:
+            joueur[0].send(bytes(mess, encoding="utf8"))
 
-        print("\nLancement de la partie...\n")
 
     # Activité du thread
     def run(self):
         # Créez ici l'interraction server / client
-        pass
+        print("En attente de la connection de 2 joueurs..\n")
+        while self.counter < 2:
+            self.client,self.addr = self.server.accept()
+            self.joueurs.append([self.client, self.addr, self.counter])
+            self.counter+=1
+            print(f"Il y a {self.counter} joueur(s) connectés")
+        print("\nLancement de la partie...\n")
+        self.sendAllPeople("lancement")
+        Main.run()
+        GameController.IA()
 
-app = App().start()
+
+app = Server().start()
