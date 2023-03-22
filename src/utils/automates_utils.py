77fe7068117829eb1eb4e174bd726f278acaed3f
Antorakk <95444282+Antorakk@users.noreply.github.com>
2023-03-22 23:30:47 +0100

    Update minmax , now work well

--- src/utils/automates_utils.py
+++ src/utils/automates_utils.py
@@ -6,6 +6,7 @@ from copy import deepcopy
 from utils.tree import Tree
 from utils.tree import evaluateGame
 import math
+from time import sleep
 
 def medium_automate(joueurActuel : Player, plateau : Plateau, index : int, view):
 
@@ -27,57 +28,145 @@ class Leaf():
         self.plateau : Plateau = plateau
         self.indexJoueur : int = indexJoueur 
         self.joueur : Player = joueur
-        # self.score = gameManager.evaluateGame(self.plateau, self.indexJoueur,self.plateau)
-
-
-        # print(self,self.parent,self.score)
 
     def makeMove(self,view):
 
         bestScore = -math.inf
         bestMove = None
+        idBestPiece = None
         pos = gameManager.getBestPossibilities(self.plateau,self.indexJoueur,self.joueur)
         print('Possibilité ----> ',pos)
 
         for i in range(len(pos)):
             # plateau = deepcopy(self.plateau)
             # index = deepcopy(self.indexJoueur)
-            joueur = deepcopy(self.joueur)
-            score = self.minmax(False,self.plateau,joueur)
+            score,idPiece = self.minmax(False,deepcopy(self.plateau),deepcopy(self.joueur))
             if score>bestScore:
                 bestScore=score
                 bestMove = pos[i]
+                idBestPiece = idPiece
         
+        # print(self.plateau)
         print(bestScore)
-        print("Meilleur position à jouer : ",bestMove)
+        print("Meilleur position à jouer : ",bestMove,"Meilleur pièce à jouer: ",idBestPiece)
         cheminFichierPiece = "./media/pieces/" + self.joueur.getCouleur().upper()[0] + "/1.png"
         
-        # for xpos,ypos in bestMove:
-        xpos,ypos= bestMove
-        self.plateau.setColorOfCase(xpos,ypos,self.indexJoueur)
-        view._addToGrid(cheminFichierPiece,ypos, xpos)
-
-
+        piece = self.joueur.jouerPiece(idBestPiece[0])
+        pieceBlokus = coordsBlocs(piece,bestMove[1],bestMove[0])
+        for xpos,ypos in pieceBlokus:
+            self.plateau.setColorOfCase(xpos,ypos,self.indexJoueur)
+            view._addToGrid(cheminFichierPiece,ypos, xpos)
+        self.joueur.hasPlayedPiece(idBestPiece[0])
 
     def minmax(self,isMaxTurn,plateau:Plateau,joueur:Player,depth=0,maxDepth=2):
 
+        
         if depth>maxDepth:
-            return gameManager.evaluateGame(plateau,self.indexJoueur,joueur)
+            return gameManager.evaluateGame(plateau,self.indexJoueur,joueur),deepcopy(joueur.logPieces)
         
         scores = []
         pos = gameManager.getBestPossibilities(plateau,self.indexJoueur,joueur)
         for piece in joueur.pieces.pieces_joueurs:
             for i in range(len(pos)):
                 check = gameManager.canPlacePiece(piece,plateau,pos[i][0],pos[i][1],joueur)
-                if check[0]!=-1:
+                if check[0]!=-1 and piece not in joueur.logPieces:
                     x,y = pos[i]
-                    pieceBlokus = coordsBlocs(joueur.jouerPiece(piece),x,y)
+                    pieceBlokus = coordsBlocs(joueur.jouerPiece(piece),y,x)
                     for xpos,ypos in pieceBlokus:
-                        plateau.setColorOfCase(xpos,ypos,self.indexJoueur)               
-                    plateau.undoMove()   
-                scores.append(self.minmax(not isMaxTurn,plateau,joueur,depth+1,maxDepth))
+                        plateau.setColorOfCase(xpos,ypos,self.indexJoueur)             
+                    # joueur.hasPlayedPiece(piece)
+                    joueur.logPieces.append(piece)
+                    scores.append(self.minmax(not isMaxTurn,plateau,joueur,depth+1,maxDepth))
+                    plateau.undoMove()  
+                    joueur.logPieces.pop()
 
-        return max(scores) if isMaxTurn else min(scores)       
+        logs = []
+        for i in range(len(scores)):
+            logs.append(scores[i][0])
+        
+        print(logs)
+        print("Max:",max(logs))
+        print(logs.index(max(logs)))
+        print(scores)
+
+        return scores[logs.index(max(logs))] if isMaxTurn else scores[logs.index(min(logs))]
+    
+    # def makeMove(self,view):
+
+    #     bestScore = -math.inf
+    #     bestMove = None
+    #     pos = gameManager.getBestPossibilities(self.plateau,self.indexJoueur,self.joueur)
+    #     print('Possibilité ----> ',pos)
+
+    #     for i in range(len(pos)):
+    #         # index = deepcopy(self.indexJoueur)
+    #         joueur = deepcopy(self.joueur)
+    #         score = self.minmax(False,self.plateau,joueur,-math.inf,math.inf)
+    #         if score > bestScore:
+    #             bestScore = score
+    #             bestMove = pos[i]
+        
+    #     print(bestScore)
+    #     print("Meilleur position à jouer : ",bestMove)
+    #     cheminFichierPiece = "./media/pieces/" + self.joueur.getCouleur().upper()[0] + "/1.png"
+        
+    #     # for xpos,ypos in bestMove:
+    #     xpos,ypos= bestMove
+    #     self.plateau.setColorOfCase(xpos,ypos,self.indexJoueur)
+    #     view._addToGrid(cheminFichierPiece,ypos, xpos)
+
+
+
+    # def minmax(self,isMaxTurn,plateau:Plateau,joueur:Player,alpha,beta,depth=0,maxDepth=2):
+        
+    #     if depth>maxDepth:
+    #         print("test")
+    #         return gameManager.evaluateGame(plateau,self.indexJoueur,joueur)
+        
+    #     scores = []
+    #     pos = gameManager.getBestPossibilities(plateau,self.indexJoueur,joueur)
+    #     if isMaxTurn:
+    #         bestVal = -math.inf
+    #         for piece in joueur.pieces.pieces_joueurs:
+    #             for i in range(len(pos)):
+    #                 check = gameManager.canPlacePiece(piece,plateau,pos[i][0],pos[i][1],joueur)
+    #                 if check[0]!=-1:
+    #                     x,y = pos[i]
+    #                     pieceBlokus = coordsBlocs(joueur.jouerPiece(piece),x,y)
+    #                     for xpos,ypos in pieceBlokus:
+    #                         plateau.setColorOfCase(xpos,ypos,self.indexJoueur)               
+    #                     plateau.undoMove()
+    #                     joueur.hasPlayedPiece(piece)
+    #                     joueur.removePiece(piece)
+    #                 scores.append(self.minmax(False,plateau,joueur,depth+1,maxDepth,alpha,beta))
+    #                 bestVal = max(bestVal,max(scores))
+    #                 alpha = max(alpha,bestVal)
+    #                 if beta<= alpha:
+    #                     print("Qd c le maximum : ",beta,alpha)
+    #                     break
+    #             return bestVal 
+    #     else:
+    #         bestVal = math.inf
+    #         for piece in joueur.pieces.pieces_joueurs:
+    #             for i in range(len(pos)):
+    #                 check = gameManager.canPlacePiece(piece,plateau,pos[i][0],pos[i][1],joueur)
+    #                 if check[0]!=-1:
+    #                     x,y = pos[i]
+    #                     pieceBlokus = coordsBlocs(joueur.jouerPiece(piece),x,y)
+    #                     for xpos,ypos in pieceBlokus:
+    #                         plateau.setColorOfCase(xpos,ypos,self.indexJoueur)               
+    #                     plateau.undoMove()
+    #                     joueur.hasPlayedPiece(piece)
+    #                     joueur.removePiece(piece)
+    #                 scores.append(self.minmax(True,plateau,joueur,depth+1,maxDepth,alpha,beta))
+    #                 bestVal = min(bestVal,min(scores))
+    #                 beta = min(beta,bestVal)
+    #                 if beta<= alpha:
+    #                     print("Qd c le minimum : ",beta,alpha)
+    #                     break
+
+
+    #             return bestVal      
     
 TAILLE = 19
 class gameManager:
@@ -112,7 +201,6 @@ class gameManager:
 
     @staticmethod
     def evaluateGame(plateau:Plateau,indexJoueur:int,joueur:Player):
-
         def iterateGrid(grid):
             for row in grid:
                 for cell in row:
@@ -123,7 +211,6 @@ class gameManager:
         for cell in iterateGrid(grid):
             if cell == indexJoueur:
                 score += 1
-        # print("------> Evaluation : ", score + len(gameManager.getBestPossibilities(plateau,indexJoueur,joueur)))
         return score + len(gameManager.getBestPossibilities(plateau,indexJoueur,joueur))
 
 
