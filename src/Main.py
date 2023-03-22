355d5821c2ce753670ae00dea0ca3e6cc4bda070
Fredericdrnl <114562911+Fredericdrnl@users.noreply.github.com>
2023-03-22 16:02:52 +0100

    add APP_PATH

--- src/Main.py
+++ src/Main.py
@@ -2,6 +2,7 @@
 from core.Core import Core
 from customtkinter import CTk
 from utils.window_utils import _resizeWindow
+from config import APP_PATH
 
 class Main:
     @staticmethod    
@@ -10,9 +11,9 @@ class Main:
             window = CTk()
             _resizeWindow(window, 700, 700)
             window.title("Blokus")
-            window.iconbitmap(r'media\Icon\icon.ico')
+            window.iconbitmap(APP_PATH + r'\..\media\Icon\icon.ico')
             app = Core.openController("Game", window)
-            app.main()
+            app.main()  
             window.mainloop()
         except Exception as e:
             print(str(e))
