92b0309262f8cd24b688d997bcb4522779985a19
Fredericdrnl <fredericdournel@gmail.com>
2023-04-06 23:06:45 +0200

    feat(MultiplayerController) : Add turn label
    Co-Authored-By: Anthony ELUECQUE <95444282+Antorakk@users.noreply.github.com>

--- src/views/GameMultiplayerView.py
+++ src/views/GameMultiplayerView.py
@@ -1,6 +1,6 @@
 from views.View import View
 from utils.window_utils import _resizeWindow, _deleteChilds, _createFrame
-from customtkinter import CTk, CTkImage, CTkLabel, CTkCanvas
+from customtkinter import CTk, CTkImage, CTkFrame, CTkLabel, CTkFont
 from PIL import Image
 from components.game.grille import grille
 from components.game.score import score
@@ -41,23 +41,28 @@ class GameMultiplayerView(View):
     def _callComponents(self):  
         self._makeFrame()
         self._makeBackground()
-
         self.grille: grille = grille(self.window, 600, 600,True)
         self.score: score = score(self.window, Player('Bleu'))
         self.piecesManager: piecesManager = piecesManager(self.window, Player('Bleu'), self)
-        
-    def __createButtons(self):
 
+    def __createButtons(self):
         # self.newGameButton: Bouton = Bouton(self.window, self, 710, 690, width=180, heigth=60, file= APP_PATH + r"/../media/assets/Button_new_game.png", son="button", command=self._newGame)
         self.leaveButton: Bouton = Bouton(self.window, self, 1060, 690, width=180, heigth=60, file= APP_PATH + r"/../media/assets/button_leave_game.png", son="button", command=self._leaveGame)
+        self.tourLabel = CTkLabel(
+        master=self.window,
+        text="", 
+        font= CTkFont(family="Roboto Medium", size=20),
+        text_color="black",
+        width=180, 
+        height=60,
+        fg_color="white",
+        bg_color="white"
+        )   
+        self.tourLabel.place(x = 710, y = 690)
 
     def _makeBackground(self):
         self.bgImage = CTkImage(Image.open(APP_PATH + r"/../media/assets/background_game.png"), size=(1300, 800))
         self.bg = CTkLabel(self.window, text="", image = self.bgImage)
-
-    def _waitingScreen(self):
-        self.waitingLabel = CTkLabel(self.window, width=1300, height=800, text="",wraplength=1)
-        self.waitingLabel.place(x = 0, y = 0)
         
     def _makePopup(self , player : Player):
         self.popup = showinfo("Blokus", "Le joueur " + player.getCouleur() + " ne peut plus jouer.")
@@ -89,7 +94,6 @@ class GameMultiplayerView(View):
         self._callComponents()
         self.__createButtons()
         self.bg.place(x = 0, y = 0)
-        # self._waitingScreen()
 
 
     def close(self):
