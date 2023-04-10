cec716f587155510779645aeaf04781fe23da724
Antorakk <95444282+Antorakk@users.noreply.github.com>
2023-04-10 20:12:09 +0200

    Need to be change to merge

--- src/Main.py
+++ src/Main.py
@@ -2,20 +2,21 @@
 from core.Core import Core
 from customtkinter import CTk
 from utils.window_utils import _resizeWindow
+from config import APP_PATH
 
 class Main:
     @staticmethod    
     def run():
-        try:
+        # try:
             window = CTk()
             _resizeWindow(window, 700, 700)
             window.title("Blokus")
-            #window.iconbitmap(r'media\Icon\icon.ico')
-            app = Core.openController("Game", window)
-            app.main()
+            window.iconbitmap(APP_PATH + r'\..\media\Icon\icon.ico')
+            app = Core.openController("Home", window)
+            app.main()  
             window.mainloop()
-        except Exception as e:
-            print(str(e))
+        # except Exception as e:
+        #     print(str(e))
 
 if __name__ == '__main__':
     Main.run()
